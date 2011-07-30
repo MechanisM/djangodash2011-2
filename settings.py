43cfaee9c6212ab31647daf4c1391c26f5882deb
Corey Oordt <coordt@washingtontimes.com>
2011-07-30 14:35:04 -0400

    Initial import

---
+++ settings.py
@@ -0,0 +1,99 @@
+# Django settings for project project.
+
+import calloway
+import os
+import sys
+
+CALLOWAY_ROOT = os.path.abspath(os.path.dirname(calloway.__file__))
+sys.path.insert(0, os.path.join(CALLOWAY_ROOT, 'apps'))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'lib'))
+sys.path.append(os.path.abspath('/Users/coordt/.virtualenvs/feedthedocs/src/readthedocs/readthedocs'))
+
+try:
+    from local_settings import DEBUG as LOCAL_DEBUG
+    DEBUG = LOCAL_DEBUG
+except ImportError:
+    DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+from calloway.settings import *
+
+ADMINS = (
+    ('coordt', 'me@you.com'),
+)
+MANAGERS = ADMINS
+DEFAULT_FROM_EMAIL='me@you.com'
+SERVER_EMAIL='me@you.com'
+
+SECRET_KEY = 'jk21v&^9tk8rh1g97qizc%=hj_ybexnh^k(#m!ueyx-!b-p=!z'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': 'dev.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'America/New_York'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+
+try:
+    from local_settings import MEDIA_URL_PREFIX
+except ImportError:
+    MEDIA_URL_PREFIX = "/media/"
+try:
+    from local_settings import MEDIA_ROOT_PREFIX
+except ImportError:
+    MEDIA_ROOT_PREFIX = os.path.join(PROJECT_ROOT, 'media')
+try:    
+    from local_settings import MEDIA_ROOT
+except ImportError:
+    MEDIA_ROOT = os.path.join(MEDIA_ROOT_PREFIX, 'uploads')
+try:
+    from local_settings import STATIC_ROOT
+except ImportError:
+    STATIC_ROOT = os.path.join(MEDIA_ROOT_PREFIX, 'static')
+    
+
+MEDIA_URL = '%suploads/' % MEDIA_URL_PREFIX
+STATIC_URL = "%sstatic/" % MEDIA_URL_PREFIX
+
+MMEDIA_DEFAULT_STORAGE = 'media_storage.MediaStorage'
+MMEDIA_IMAGE_UPLOAD_TO = 'image/%Y/%m/%d'
+
+AUTH_PROFILE_MODULE = ''
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
+) + CALLOWAY_TEMPLATE_DIRS
+
+CACHE_BACKEND = 'memcached://localhost:11211/'
+
+INSTALLED_APPS = APPS_DJANGO_BASE + \
+    APPS_MESSAGES + \
+    APPS_ADMIN + (
+        'staticfiles',
+        'calloway',
+    )
+
+ADMIN_TOOLS_THEMING_CSS = 'calloway/admin/css/theming.css'
+# ADMIN_TOOLS_MENU = 'menu.CustomMenu'
+
+TINYMCE_JS_URL = '%scalloway/js/tiny_mce/tiny_mce.js' % STATIC_URL
+TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, 'js/tiny_mce')
+
+from readthedocs.settings.sqlite import *
+# try:
+#     from local_settings import *
+# except ImportError:
+#     pass
---
+++ urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import os
+from django.conf import settings
+
+admin.autodiscover()
+
+handler500 = 'django_ext.views.custom_server_error'
+
+sitemaps = {
+}
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+)
+from calloway.urls import urlpatterns as calloway_patterns
+
+urlpatterns += calloway_patterns
+
+from readthedocs.urls import urlpatterns as readthedocs_patterns
+urlpatterns += readthedocs_patterns
+
+# if settings.DEBUG:
+#     urlpatterns += patterns('',
+#         (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+#             {'document_root': os.path.join(os.path.dirname(__file__), 'media2')}),
+#     )
